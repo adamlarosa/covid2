8b890f914f21a38008507760780e83a70e0ddb2b
Adam La Rosa <adam.larosa@gmail.com>
2020-05-15 13:42:35 -0700

    finished sorting json

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
 	componentDidMount() {
 		this.getCases(this.state.slug);
 	}
-
 	getCases = async (slug) => {
 		await fetch(`https://api.covid19api.com/dayone/country/${slug}`)
 			.then(resp => resp.json())
@@ -22,6 +21,61 @@ class App extends Component {
 			})
 		await console.log("download complete")
 	}
+
+
+	provinceInStates = (entry) => {
+		const { states } = this.state
+		return Object.keys(states).includes(entry.Province)
+	}
+
+	countyInState = (entry) => {
+		const { states } = this.state
+		return Object.keys(states[entry["Province"]]).includes(entry.City)
+	}
+
+	addEntryToState = (entry) => {
+		const { Province, City } = entry;
+		let newStates = this.state.states
+		newStates[Province][City].push(entry) 
+	}
+
+	createEntryInState = (entry) => {
+		const { Province, City } = entry
+		let newStates = this.state.states
+		newStates[Province] = {}
+		newStates[Province][City] = []
+		newStates[Province][City].push(entry);
+		this.setState({ states: newStates })
+	}
+
+	createCountyInState = (entry) => {
+		const { Province, City } = entry;
+		let newStates = this.state.states;
+		newStates[Province][City] = []
+		newStates[Province][City].push(entry)
+		this.setState({ states: newStates })
+	}
+
+	addDataToStates = (data) => {
+		const { provinceInStates, countyInState } = this
+		for (const entry in data) {
+			if (provinceInStates(data[entry])) {
+				if (countyInState(data[entry])) {
+					this.addEntryToState(data[entry])
+				} else {
+					this.createCountyInState(data[entry])
+				}
+			} else {
+				this.createEntryInState(data[entry]);
+			}
+		}
+	}
+
+
+
+
+
+
 
 
 	topTest = () => {
@@ -35,17 +89,23 @@ class App extends Component {
 				</header>
 
 				<main className="App-main">
+
 					<button
 						onClick={() => this.topTest()}
 					>
 						SHOWSTATE
-					</button> <br/>
-					main<br/>
+					</button> 
+					
+					<br/>
+					main
+					<br/>
+					
 					<button
-						onClick={() => console.log("no test set up")}
+						onClick={() => this.addDataToStates(this.state.data)}
 					>
-						-!-
+						ADD DATA TO STATES
 					</button>
+					
 				</main>
 
 			</div>
